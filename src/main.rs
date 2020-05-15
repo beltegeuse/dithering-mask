a3ecc39bf4995a606ecf11697f6b32f87bb953af
beltegeuse <adrien.gruson@gmail.com>
2020-05-15 00:58:01 -0400

    add command line, refactoring and naive parallel processing (via rayon)

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,35 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
 use std::time;
+// Command line
+use clap::*;
+// LDR image (png)
+use image::*;
+// Parallel processing
+use rayon::prelude::*;
 
 // Functions to write output mask
+pub fn save_ldr_image(img: &[f32], size: (usize, usize), imgout_path_str: &str) {
+    let mut image_ldr = DynamicImage::new_rgb8(size.0 as u32, size.1 as u32);
+    for x in 0..size.0 {
+        for y in 0..size.1 {
+            let p = img[y * size.0 + x];
+            image_ldr.put_pixel(
+                x as u32,
+                y as u32,
+                Rgba::from_channels(
+                    (p.min(1.0).powf(1.0 / 2.2) * 255.0) as u8,
+                    (p.min(1.0).powf(1.0 / 2.2) * 255.0) as u8,
+                    (p.min(1.0).powf(1.0 / 2.2) * 255.0) as u8,
+                    255,
+                ),
+            );
+        }
+    }
+    image_ldr
+        .save(&Path::new(imgout_path_str))
+        .expect("failed to write img into file");
+}
 fn save_pfm(img: &[f32], size: (usize, usize), imgout_path_str: &str) {
     let file = File::create(Path::new(imgout_path_str)).unwrap();
     let mut file = BufWriter::new(file);
@@ -41,6 +68,28 @@ fn save_pfm_2d(r: &[f32], g: &[f32], size: (usize, usize), imgout_path_str: &str
         }
     }
 }
+pub fn save_ldr_image_2d(r: &[f32], g: &[f32], size: (usize, usize), imgout_path_str: &str) {
+    let mut image_ldr = DynamicImage::new_rgb8(size.0 as u32, size.1 as u32);
+    for x in 0..size.0 {
+        for y in 0..size.1 {
+            let r_v = r[y * size.0 + x];
+            let g_v = g[y * size.0 + x];
+            image_ldr.put_pixel(
+                x as u32,
+                y as u32,
+                Rgba::from_channels(
+                    (r_v.min(1.0).powf(1.0 / 2.2) * 255.0) as u8,
+                    (g_v.min(1.0).powf(1.0 / 2.2) * 255.0) as u8,
+                    0,
+                    255,
+                ),
+            );
+        }
+    }
+    image_ldr
+        .save(&Path::new(imgout_path_str))
+        .expect("failed to write img into file");
+}
 
 fn main() {
     // Setup logger
@@ -49,57 +98,135 @@ fn main() {
         .parse_filters("info")
         .init();
 
-    // Constants
-    // TODO: Use clap for make it more user friendly
-    const SIZE: i32 = 32;
-    const ITER: i32 = 1024;
-    const SAVE_ITER: i32 = 32;
-    const DIM: i32 = 2;
+    let matches =
+        App::new("dithering-mask")
+            .version("0.1.0")
+            .author("Adrien Gruson <adrien.gruson@gmail.com>")
+            .about("A Rusty Implementation of Mask generation - \"Blue-noise dithered sampling\", Georgiev and Fajardo, 2016")
+            .arg(
+                Arg::with_name("size")
+                    .required(true)
+                    .takes_value(true)
+                    .help("image size"),
+            )
+            .arg(
+                Arg::with_name("dimension")
+                    .required(true)
+                    .takes_value(true)
+                    .help("mask dimension"),
+            )
+            .arg(
+                Arg::with_name("output")
+                    .required(true)
+                    .takes_value(true)
+                    .help("output name (.pfm)"),
+            )
+            .arg(
+                Arg::with_name("intermediate")
+                    .short("x")
+                    .takes_value(true)
+                    .help("frequency of intermediate output (debug)"),
+            )
+            .arg(
+                Arg::with_name("iteration")
+                    .short("i")
+                    .takes_value(true)
+                    .help("number of iterations")
+                    .default_value("1024"),
+            )
+            .arg(
+                Arg::with_name("png")
+                    .short("p")
+                    .help("save in png (otherwise save in pfm by default)"),
+            )
+            .get_matches();
+
+    // Reading user parameters
+    // -- Global
+    let size = value_t_or_exit!(matches.value_of("size"), i32);
+    let dimension = value_t_or_exit!(matches.value_of("dimension"), i32);
+    let slice_size = (size * size) as usize;
+    let factor = dimension as f32 / 2.0;
+    // --- image output
+    let output = value_t_or_exit!(matches.value_of("output"), String);
+    let ext = if matches.is_present("png") {
+        "png"
+    } else {
+        "pfm"
+    };
+    let save_img = if matches.is_present("png") {
+        save_ldr_image
+    } else {
+        save_pfm
+    };
+    let save_img_2d = if matches.is_present("png") {
+        save_ldr_image_2d
+    } else {
+        save_pfm_2d
+    };
+    // -- Optimization
+    let iteration = value_t_or_exit!(matches.value_of("iteration"), i32);
+    // -- Debug
+    let output_iter = match matches.value_of("intermediate") {
+        None => None,
+        Some(v) => Some(v.parse::<i32>().expect(&format!("{} is not an i32", v))),
+    };
     const SIGMA_I: f32 = 2.1;
     const SIGMA_S: f32 = 1.0;
-    const FACTOR: f32 = DIM as f32 / 2.0;
-    let slice_size = (SIZE * SIZE) as usize;
-    let mut rng = rand::thread_rng();
 
+    // Create the thread pool
+    let pool = rayon::ThreadPoolBuilder::new().build().unwrap();
     // Create the random image
-    let mut img = vec![0.0 as f32; (SIZE * SIZE * DIM) as usize];
-    for i in 0..(SIZE * SIZE * DIM) as usize {
+    let mut rng = rand::thread_rng();
+    let mut img = vec![0.0 as f32; (size * size * dimension) as usize];
+    for i in 0..(size * size * dimension) as usize {
         img[i] = rng.gen_range(0.0, 1.0);
     }
     info!("{:?}", img);
 
     // Create vector of pixel coordinate [(x,y,d), ...]
-    let mut index = (0..SIZE * SIZE * DIM)
+    let mut index = (0..size * size * dimension)
         .map(|v| {
-            let v2 = v % (SIZE * SIZE);
-            (v2 % SIZE, v2 / SIZE, v / (SIZE * SIZE))
+            let v2 = v % (size * size);
+            (v2 % size, v2 / size, v / (size * size))
         })
         .collect::<Vec<_>>();
+    let index_order = index.clone();
 
     // Helper to convert (x,y,d) to img coordinates
     let get_index =
-        |p: (i32, i32, i32)| -> usize { (p.2 * SIZE * SIZE + p.1 * SIZE + p.0) as usize };
+        |p: (i32, i32, i32)| -> usize { (p.2 * size * size + p.1 * size + p.0) as usize };
 
     // Lambda that compute the energy if the pixel get swap or not
     // This information is used later for computing the probability to swap
-    let energy_diff_pair =
-        |img: &Vec<f32>, p1: (i32, i32, i32), p2: (i32, i32, i32)| -> (f32, f32) {
-            let mut e_org = 0.0;
-            let mut e_new = 0.0;
-            let p1_v = img[get_index(p1)];
-            let p2_v = img[get_index(p2)];
-            for d in 0..DIM {
-                for y in 0..SIZE {
-                    for x in 0..SIZE {
-                        if x == p1.0 && y == p1.1 && d == p1.2 {
-                            continue;
-                        }
-                        if x == p2.0 && y == p2.1 && d == p2.2 {
-                            continue;
-                        }
-
-                        let current_v = img[get_index((x, y, d))];
-
+    struct Result {
+        pub org: f32,
+        pub new: f32,
+    }
+    impl std::iter::Sum for Result {
+        fn sum<I>(iter: I) -> Self
+        where
+            I: Iterator<Item = Self>,
+        {
+            iter.fold(Self { org: 0.0, new: 0.0 }, |a, b| Self {
+                org: a.org + b.org,
+                new: a.new + b.new,
+            })
+        }
+    }
+    let energy_diff_pair = |img: &Vec<f32>, p1: (i32, i32, i32), p2: (i32, i32, i32)| -> Result {
+        let p1_v = img[get_index(p1)];
+        let p2_v = img[get_index(p2)];
+        pool.install(|| {
+            index_order
+                .par_iter()
+                .map(|(x, y, d)| {
+                    if *x == p1.0 && *y == p1.1 && *d == p1.2 {
+                        Result { org: 0.0, new: 0.0 }
+                    } else if *x == p2.0 && *y == p2.1 && *d == p2.2 {
+                        Result { org: 0.0, new: 0.0 }
+                    } else {
+                        let current_v = img[get_index((*x, *y, *d))];
                         // Here compute the cost
                         // --- Distance
                         let sqr_dist_1 = (x - p1.0).pow(2) + (y - p1.1).pow(2) + (d - p1.2).pow(2);
@@ -107,28 +234,27 @@ fn main() {
                         let sqr_dist_2 = (x - p2.0).pow(2) + (y - p2.1).pow(2) + (d - p2.2).pow(2);
                         let dist_term_2 = (-(sqr_dist_2 as f32) / SIGMA_I).exp();
                         // --- Value
-                        let value_term_1 = (-(p1_v - current_v).abs().powf(FACTOR) / SIGMA_S).exp();
-                        let value_term_2 = (-(p2_v - current_v).abs().powf(FACTOR) / SIGMA_S).exp();
+                        let value_term_1 = (-(p1_v - current_v).abs().powf(factor) / SIGMA_S).exp();
+                        let value_term_2 = (-(p2_v - current_v).abs().powf(factor) / SIGMA_S).exp();
 
-                        // Compute the orginal and the new value
-                        e_org += dist_term_1 * value_term_1;
-                        e_org += dist_term_2 * value_term_2;
-                        e_new += dist_term_1 * value_term_2;
-                        e_new += dist_term_2 * value_term_1;
+                        Result {
+                            org: dist_term_1 * value_term_1 + dist_term_2 * value_term_2,
+                            new: dist_term_1 * value_term_2 + dist_term_2 * value_term_1,
+                        }
                     }
-                }
-            }
-            (e_new, e_org)
-        };
+                })
+                .sum::<Result>()
+        })
+    };
 
     // Compute the energy for a given pixel
-    // to all the other pixel of the image 
+    // to all the other pixel of the image
     let energy_pixel = |p1: (i32, i32, i32)| -> f32 {
         let p1_v = img[get_index(p1)];
         let mut e = 0.0;
-        for d in 0..DIM {
-            for y in 0..SIZE {
-                for x in 0..SIZE {
+        for d in 0..dimension {
+            for y in 0..size {
+                for x in 0..size {
                     if x == p1.0 && y == p1.1 && d == p1.2 {
                         continue;
                     }
@@ -138,7 +264,7 @@ fn main() {
                     // --- Distance
                     let sqr_dist_1 = (x - p1.0).pow(2) + (y - p1.1).pow(2) + (d - p1.2).pow(2);
                     let dist_term_1 = (-(sqr_dist_1 as f32) / SIGMA_I).exp();
-                    let value_term_1 = (-(p1_v - current_v).abs().powf(FACTOR) / SIGMA_S).exp();
+                    let value_term_1 = (-(p1_v - current_v).abs().powf(factor) / SIGMA_S).exp();
                     // Compute the orginal and the new value
                     e += dist_term_1 * value_term_1;
                 }
@@ -147,14 +273,14 @@ fn main() {
         e
     };
 
-    // Compute the total energy 
+    // Compute the total energy
     // Note that this information is informative only
     // as total energy is never used in our optimization process
     info!("Compute total energy ...");
     let mut energy = 0.0;
-    for d in 0..DIM {
-        for y in 0..SIZE {
-            for x in 0..SIZE {
+    for d in 0..dimension {
+        for y in 0..size {
+            for x in 0..size {
                 energy += energy_pixel((x, y, d));
             }
         }
@@ -166,49 +292,51 @@ fn main() {
     // Simulated anneling
     let mut delta_avg: f32 = 1.0;
     let mut total_accepted = 0;
-    // Temperature 
+    // Temperature
     const P_1: f32 = 0.7; // Initial prob to accept a swap
-    const P_FINAL: f32 = 0.001; // Final prob to accept a swap 
+    const P_FINAL: f32 = 0.001; // Final prob to accept a swap
     let t_1: f32 = -1.0 / P_1.ln();
     let t50: f32 = -1.0 / P_FINAL.ln();
-    let frac: f32 = (t50 / t_1).powf(1.0 / (ITER - 1) as f32);
+    let frac: f32 = (t50 / t_1).powf(1.0 / (iteration - 1) as f32);
     let mut temp = t_1; // The current temperature
-    for t in 0..ITER {
+    for t in 0..iteration {
         // For computing how long take an iteration...
         let now = time::Instant::now();
-        // Save a temporay image if needed   
-        if t % SAVE_ITER == 0 {
-            if DIM == 2 {
-                save_pfm_2d(
-                    &img[0..slice_size],
-                    &img[slice_size..(2 * slice_size)],
-                    (SIZE as usize, SIZE as usize),
-                    &format!("{}_.pfm", t),
-                );
-            } else {
-                // Take the first dimension by default
-                save_pfm(
-                    &img[0..(SIZE * SIZE) as usize],
-                    (SIZE as usize, SIZE as usize),
-                    &format!("{}_.pfm", t),
-                );
+        // Save a temporay image if needed
+        if let Some(iter) = output_iter {
+            if t % iter == 0 {
+                if dimension == 2 {
+                    save_img_2d(
+                        &img[0..slice_size],
+                        &img[slice_size..(2 * slice_size)],
+                        (size as usize, size as usize),
+                        &format!("{}_{}_.{}", output, t, ext),
+                    );
+                } else {
+                    // Take the first dimension by default
+                    save_img(
+                        &img[0..(size * size) as usize],
+                        (size as usize, size as usize),
+                        &format!("{}_{}_.{}", output, t, ext),
+                    );
+                }
             }
         }
-        // Shuffle the pair 
+        // Shuffle the pair
         // At each iteration, all pixel will have a swap attempt
         index.shuffle(&mut rng);
         info!(
             "[{}/{}] \t Energy: {} \t Temp: {}",
             t + 1,
-            ITER,
+            iteration,
             energy,
             temp
         );
         let mut accepted_moves = 0;
         for chunk in index.chunks_exact(2) {
             // Compute the new and old energy if we was doing the swap
-            let (new, old) = energy_diff_pair(&img, chunk[0], chunk[1]);
-            let delta = new - old;
+            let res = energy_diff_pair(&img, chunk[0], chunk[1]);
+            let delta = res.new - res.org;
             let delta_abs = delta.abs();
             let accept = if delta < 0.0 {
                 true
@@ -244,25 +372,25 @@ fn main() {
     }
 
     // Save final (all dimension)
-    match DIM {
-        1 => save_pfm(
+    match dimension {
+        1 => save_img(
             &img[0..slice_size],
-            (SIZE as usize, SIZE as usize),
-            &"final.pfm",
+            (size as usize, size as usize),
+            &format!("{}.{}", output, ext),
         ),
-        2 => save_pfm_2d(
+        2 => save_img_2d(
             &img[0..slice_size],
-            &img[slice_size..2*slice_size],
-            (SIZE as usize, SIZE as usize),
-            &"final.pfm",
+            &img[slice_size..2 * slice_size],
+            (size as usize, size as usize),
+            &format!("{}.{}", output, ext),
         ),
         _ => {
-            for d in 0..DIM {
-                let d_beg = (d * SIZE * SIZE) as usize;
-                save_pfm(
-                    &img[d_beg..d_beg + (SIZE * SIZE) as usize],
-                    (SIZE as usize, SIZE as usize),
-                    &format!("final_{}_.pfm", d),
+            for d in 0..dimension {
+                let d_beg = (d * size * size) as usize;
+                save_img(
+                    &img[d_beg..d_beg + (size * size) as usize],
+                    (size as usize, size as usize),
+                    &format!("{}_{}_.{}", output, d, ext),
                 );
             }
         }
